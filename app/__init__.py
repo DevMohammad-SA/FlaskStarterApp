f5ff1478b74d460d84299e273355e8f9524aa120
Mohammad Albuainain <moh_dev@icloud.com>
2024-12-18 17:48:50 +0300

    Update .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    app.config['SECRET_KEY'] = 'secret_key_here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
